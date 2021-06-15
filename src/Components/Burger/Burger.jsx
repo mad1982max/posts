b4c517046467c9b7666d800c9bba02ce413aac10
maks_bezr <maks.bezrodnyi@gmail.com>
2021-06-16 00:22:49 +0300

    burger start

--- src/Components/Burger/Burger.jsx
+++ src/Components/Burger/Burger.jsx
@@ -1,9 +1,17 @@
+import "./burger.css";
+
 const Burger = () => {
+  const handleClick = (e) => {
+    const element = e.currentTarget;
+    element.classList.toggle("open");
+  };
   return (
-    <div className="burger-wrapper">
-      Burger
-    </div>
-  )
-}
+    <button onClick={handleClick} className="burger-menu-button">
+      <div className="bar-top"></div>
+      <div className="bar-mid"></div>
+      <div className="bar-bottom"></div>
+    </button>
+  );
+};
 
 export default Burger;
